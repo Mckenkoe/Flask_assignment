e71ad1e7533c812014bcedb8969eaf2632ebb58d
Mckenkoe <koetitz_mckenna@columbusstate.edu>
2022-09-22 19:32:44 +0000

    added form

--- todo.py
+++ todo.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "hello world"
+    return render_template('home.html')
+
+@app.route('/addTodo', methods=['POST'])
+def addTodo():
+    #access body info
+    text1 = request.form['text1']
+    print(text1)
+    return(text1)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
